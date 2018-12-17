64bf6fee9d3a6d5d1ef2df24d21827bf6385333b
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2018-12-17 14:16:49 +0200

    12/17/18

---
+++ qline.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'qline.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(0, 10, 791, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(0, 40, 791, 20))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_3.setGeometry(QtCore.QRect(0, 70, 791, 20))
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.lineEdit_4 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_4.setGeometry(QtCore.QRect(0, 100, 791, 20))
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        self.lineEdit_5 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_5.setGeometry(QtCore.QRect(0, 130, 791, 20))
+        self.lineEdit_5.setObjectName("lineEdit_5")
+        self.lineEdit_6 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_6.setGeometry(QtCore.QRect(0, 160, 791, 20))
+        self.lineEdit_6.setObjectName("lineEdit_6")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+
--- test.py
+++ test.py
@@ -1,9 +1,29 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, QtCore
+from qline import Ui_MainWindow
 import sys
 
 
-app = QtWidgets.QApplication([])
-win = uic.loadUi("main.ui")
+class MyWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-win.show()
+        self.ui.lineEdit.setText("Welcome on PythonScripts")
+
+        self.ui.lineEdit_2.setMaxLength(10)
+
+        self.ui.lineEdit_3.setEchoMode(QtWidgets.QLineEdit.Password)
+
+        self.ui.lineEdit_4.setReadOnly(True)
+
+        self.ui.lineEdit_5.setStyleSheet("color: rgb(28, 43, 255);")
+
+        self.ui.lineEdit_6.setStyleSheet("background-color: rgb(28, 43, 255);")
+
+
+app = QtWidgets.QApplication([])
+application = MyWindow()
+application.show()
+
 sys.exit(app.exec())
